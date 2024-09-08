0fe384087c43bb87c82e272182d2645f3409fe42
ohmfordev <sittiporn.emr@gmail.com>
2024-09-08 22:28:40 +0700

    updated 08 09

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,11 @@
-import { useState } from 'react';
-import axios from 'axios';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [apiResponse, setApiResponse] = useState('');
-
-  const handleApiRequest = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000');
-      setApiResponse(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setApiResponse('Error fetching data');
-    }
-  };
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={handleApiRequest}>
-          Make API Request
-        </button>
-        <p>{apiResponse}</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Welcome to our Product App</h1>
+      {/* เพิ่มเนื้อหาหน้าแรกของแอปพลิเคชันที่นี่ */}
+    </div>
   );
 }
 
