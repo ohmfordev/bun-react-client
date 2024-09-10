e1916078e4620fe835d0ccad2c2022bf9c326119
ohmfordev <sittiporn.emr@gmail.com>
2024-09-10 07:34:45 +0700

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRouter from './';
+import AppRouter from './Router';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') ).render(
